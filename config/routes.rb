3ec0913596423c54e7c7ce15fa2ac8af8d2d6d72
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-05 15:47:20 -0500

    create teacher controller so I can create the upload method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root      'teacher#index'
   resources :teachers
   resources :students
 
