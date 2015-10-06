88fed53e63606a5d7544c6e3d59d842997e76129
Harrison Klucher & Matt Behan <pair+hklucher.mattbehan@devbootcamp.com>
2015-10-06 13:21:40 -0500

    start views for students

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :first_name
     devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
+  
 end
