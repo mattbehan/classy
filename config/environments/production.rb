9ad999e371455329fedf0bb42ede29becf556e85
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-06 17:23:23 -0500

    trying to configure email with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+  config.action_mailer.default_url_options = { :host => 'https://secret-coast-8313.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['pdgonzalez872'],
+    :password       => ENV['testing1234'],
+    :domain         => ENV['heroku.com']
+  }
+
+
 end
