9a5c3127739e91ddf62e97de677ff0af13f49482
Harrison Klucher & Matt Behan <pair+hklucher.mattbehan@devbootcamp.com>
2015-10-06 12:51:19 -0500

    add before filter to authenticate teacher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_teacher!
   protect_from_forgery with: :exception
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name 
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
 end
