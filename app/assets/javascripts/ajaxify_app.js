b6b4408b09d5a49451f5e312395881e951b3c5fb
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-10-06 10:33:58 -0500

    Implement ajax to csv upload, will work on begin end rescue stuff

--- app/assets/javascripts/ajaxify_app.js
+++ app/assets/javascripts/ajaxify_app.js
@@ -1,1 +1,47 @@
-console.log("from ajaxify")
+// console.log("from ajaxify")
+
+
+// function addDetention () {
+//   $("form").on("submit", function(event){
+//     event.preventDefault();
+//     console.log("binded");
+//
+//     console.log(event)
+//
+//   });
+// }
+
+function uploadCSV () {
+  $(document).on("submit", "form", function(event){
+    event.preventDefault();
+
+    console.log("bound");
+
+    var data;
+
+    data = new FormData();
+    data.append('file', $('#file')[0].files[0]);
+
+    console.log(data);
+
+
+    var request = $.ajax({
+      type: 'post',
+      url: '/teachers/upload',
+      data: data,
+      processData: false,
+      contentType: false,
+    });
+
+    request.done(function(response){
+      console.log("response done")
+    });
+
+    request.error(function(response) {
+      console.log("error");
+      console.log(response);
+    });
+
+
+  });
+}
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -11,20 +11,42 @@ class TeachersController < ApplicationController
   end
 
   def upload
-    uploaded_file = params[:file]
-    file_content = uploaded_file.read
-    students = CSV.parse(file_content)
-    students[1..-1].each do |student|
-      s = Student.new(first_name:   student[0].to_s, # first_name
-                      last_name:    student[1].to_s, # last_name
-                      gender:       student[2].to_s,
-                      grade:        student[3].to_i, # grade
-                      gpa:          student[4].to_f, # gpa
-                      detentions:   student[5].to_i, # detentions
-                      t_shirt_size: student[6].to_s) # t_shirt_size
-      s.save
+
+    if request.xhr?
+      # raise params[:file]
+      p params[:file]
+      p "hi"
+
+      uploaded_file = params[:file]
+      file_content = uploaded_file.read
+      students = CSV.parse(file_content)
+      students[1..-1].each do |student|
+        s = Student.new(first_name:   student[0].to_s, # first_name
+                        last_name:    student[1].to_s, # last_name
+                        gender:       student[2].to_s,
+                        grade:        student[3].to_i, # grade
+                        gpa:          student[4].to_f, # gpa
+                        detentions:   student[5].to_i, # detentions
+                        t_shirt_size: student[6].to_s) # t_shirt_size
+        s.save
+      end
+      redirect_to root_url
     end
-    redirect_to root_url
+
+    # uploaded_file = params[:file]
+    # file_content = uploaded_file.read
+    # students = CSV.parse(file_content)
+    # students[1..-1].each do |student|
+    #   s = Student.new(first_name:   student[0].to_s, # first_name
+    #                   last_name:    student[1].to_s, # last_name
+    #                   gender:       student[2].to_s,
+    #                   grade:        student[3].to_i, # grade
+    #                   gpa:          student[4].to_f, # gpa
+    #                   detentions:   student[5].to_i, # detentions
+    #                   t_shirt_size: student[6].to_s) # t_shirt_size
+    #   s.save
+    # end
+    # redirect_to root_url
   end
 
 end
