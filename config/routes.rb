76287f2722f4bc20b116eb307d655cf5e807a5b1
Harrison Klucher & Matt Behan <pair+hklucher.mattbehan@devbootcamp.com>
2015-10-06 20:16:12 -0500

    fix custom add new teacher route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :teachers
 
+  devise_scope :teacher do
+    get "sign_up", to: "teachers#new_session"
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
