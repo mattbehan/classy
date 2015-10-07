8e8d4354fb0e41b631c50b4d3e5d6c7057da7d73
Matt Behan <email": null>
2015-10-06 19:28:18 -0500

    resolve port issue with mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://secret-coast-8313.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
-    :port           => 587,
+    :port           => 25,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
